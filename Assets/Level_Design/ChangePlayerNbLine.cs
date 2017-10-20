9241500cbe4379cfa7d53719d8171bdc4de947bf
Unknown <simon.delavenne@hotmail.fr>
2017-10-20 16:39:32 +0200

    WIP_LevelTuto + LevelTest
    Prefabs du niveau tutoriels et des niveaux de test (mini niveau mettant en scène les nouveaux ennemis) + prefabs des situations de jeux

---
+++ Assets/Level_Design/ChangePlayerNbLine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayerNbLine : MonoBehaviour {
+
+	public int nbLine ;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<PlayerController>().NbrLine = nbLine ;
+		}
+	}
+}
